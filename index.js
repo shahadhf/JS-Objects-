dec848bdd0b7abb596234da7c5d312639518f104
shahadhf <salmarag@outlook.com>
2024-11-05 16:20:58 +0300

    done

---
+++ index.js
@@ -0,0 +1,36 @@
+/* PART 1 */
+
+// Q1
+
+const book = {
+  title: "JavaScript: The Definitive Guide",
+  author: "David Flanagan",
+  publishedYear: 2020,
+  genre: "Programming",
+};
+
+//Q2
+console.log(book.title, book.publishedYear);
+
+/* PART 2 */
+
+//Q3
+book.pageCount = 1096;
+
+//Q4
+book.ISBN = "978-1491952023";
+
+//Q5
+book.publishedYear = 2021;
+
+/* PART 3 */
+
+//Q6
+
+book.author = ["David Flanagan", "Another Author"];
+
+//Q7
+
+book.reviews = [
+  { reviewer: "Book Critic", comment: "A comprehensive guide to JavaScript." },
+];
